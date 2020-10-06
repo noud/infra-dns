de646d414b24cbceb25bdfe28cf35a8cbb616479
Noud de Brouwer <noud4@home.nl>
2020-10-06 22:26:51 +0200

    netplan

--- bin/install-config-host1.sh
+++ bin/install-config-host1.sh
@@ -2,4 +2,7 @@
 
 export HOST=host1
 
+sudo cp etc/${HOST}/netplan/02-static-address.yaml /etc/netplan
+sudo cp etc/${HOST}/netplan/00-private-nameservers.yaml /etc/netplan
+sudo netplan apply
 sudo cp etc/${HOST}/resolv.conf /etc
