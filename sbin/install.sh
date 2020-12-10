7e10e1fb97b623dec2dc488338c99c684f616562
Noud de Brouwer <noud4@home.nl>
2020-12-10 14:29:00 +0100

    ansible gnome dconf

--- sbin/install.sh
+++ sbin/install.sh
@@ -6,3 +6,4 @@ sudo apt update -y
 sudo apt install -y ansible
 ansible-galaxy collection install ansible.posix
 ansible-galaxy collection install community.general
+ansible-galaxy install petermosmans.customize-gnome
