dca5efced7a262649526db44cac001bb5654aa83
Noud de Brouwer <noud4@home.nl>
2020-12-19 13:03:21 +0100

    ansible

--- sbin/install.sh
+++ sbin/install.sh
@@ -1,8 +1,7 @@
-#/usr/bin/env sh
+#/usr/bin/env bash
 
 sudo apt upgrade -y
-sudo apt update -y
-
+sudo apt update -y --allow-releaseinfo-change
 sudo apt install -y git
 
 sudo apt install -y ansible
@@ -10,5 +9,17 @@ ansible-galaxy collection install ansible.posix
 ansible-galaxy collection install community.general
 ansible-galaxy install petermosmans.customize-gnome
 
-mkdir -p $USER/.ansible/roles
-cd $USER/.ansible/roles && git clone https://github.com/PeterMosmans/ansible-role-customize-gnome.git
+mkdir -p $USER/.ansible/roles && cd $USER/.ansible/roles && git clone https://github.com/PeterMosmans/ansible-role-customize-gnome.git
+
+declare -a arr=("packages" \
+"infra" \
+"develop" \
+"docker"
+)
+
+cd ~/workspaces/laravel-workspace/infra-dns
+for i in "${arr[@]}"
+do
+    export PLAYBOOK="$i"
+    ansible-playbook -i domain ${PLAYBOOK}.yml --ask-vault-pass 
+done
