70ede06aa206e7ad9a9a6c5a1c366c5b4c3a26dc
noud <noud4@home.nl>
2021-03-13 00:15:44 +0100

    vscode

--- sbin/install.sh
+++ sbin/install.sh
@@ -9,6 +9,7 @@ ansible-galaxy collection install ansible.posix
 ansible-galaxy collection install community.general
 ansible-galaxy install petermosmans.customize-gnome
 ansible-galaxy install wahidsadik.apache-virtual-host
+ansible-galaxy install gantsign.visual-studio-code-extensions
 
 mkdir -p $USER/.ansible/roles && cd $USER/.ansible/roles && git clone https://github.com/PeterMosmans/ansible-role-customize-gnome.git
 
